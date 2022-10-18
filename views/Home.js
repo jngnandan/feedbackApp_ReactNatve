58cdc074a77b1df4b051d0e8b1c19acd94460173
jngnandan <jngnandan@gmail.com>
2022-10-18 15:51:59 +0100

    feedback added

--- views/Home.js
+++ views/Home.js
@@ -3,22 +3,22 @@ import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 import React from 'react'
 
 const Home = () => {
-    const [state, setState] = React.useState('very satisfied')
+    const [state, setState] = React.useState('')
 
     const home = () => {
         return (
      <View style={{height: 200, backgroundColor: 'white', width: '94%', flexDirection:'column', justifyContent: 'center', alignItems: 'center', borderRadius: 6}}>
       <Text style={{fontSize: 20, fontWeight: '600', padding: 14}}>How satisfyed are you with our customer support performance?</Text>
       <View style={{flex: 1, flexDirection:'row',}}>
-        <TouchableOpacity style={{alignItems: 'center', width: 105}}>
+        <TouchableOpacity onPress={() => setState('verySatisfied')} style={{alignItems: 'center', width: 105}}>
         <Text style={{fontSize: 50}}>😊</Text>
         <Text>Very Satisfied</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={{alignItems: 'center', width: 105}}>
+        <TouchableOpacity onPress={() => setState('verySad')} style={{alignItems: 'center', width: 105}}>
         <Text style={{fontSize: 50}}>😭</Text>
         <Text>Very Sad</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={{alignItems: 'center', width: 105}}>
+        <TouchableOpacity onPress={() => setState('verySad')} style={{alignItems: 'center', width: 105}}>
         <Text style={{fontSize: 50}}>😟</Text>
         <Text>Not Bad</Text>
         </TouchableOpacity>
@@ -27,9 +27,53 @@ const Home = () => {
         )
     }
 
+    const verySatisfied = () => {
+        return (
+     <View style={{height: 200, backgroundColor: 'white', width:'99%', flexDirection:'column', justifyContent: 'center', alignItems: 'center', borderRadius: 6}}>
+      <Text style={{fontSize: 20, fontWeight: '600', padding: 14}}>Thank you for your feedback!</Text>
+      <View style={{flex: 1, flexDirection:'row',}}>
+        <TouchableOpacity onPress={() => setState('')} style={{alignItems: 'center', width: 105}}>
+        <Text style={{fontSize: 50}}>😊</Text>
+        <Text>Very Satisfied</Text>
+        </TouchableOpacity>
+      </View> 
+    </View>
+        )
+    }
+
+        const verySad = () => {
+        return (
+     <View style={{height: 200, backgroundColor: 'white', width:'99%', flexDirection:'column', justifyContent: 'center', alignItems: 'center', borderRadius: 6}}>
+      <Text style={{fontSize: 20, fontWeight: '600', padding: 14}}>Thank you for your feedback!</Text>
+      <View style={{flex: 1, flexDirection:'row',}}>
+        <TouchableOpacity onPress={() => setState('')} style={{alignItems: 'center', width: 105}}>
+        <Text style={{fontSize: 50}}>😊</Text>
+        <Text>Not Bad</Text>
+        </TouchableOpacity>
+      </View> 
+    </View>
+        )
+    }
+
+const notBad = () => {
+        return (
+     <View style={{height: 200, backgroundColor: 'white', width:'99%', flexDirection:'column', justifyContent: 'center', alignItems: 'center', borderRadius: 6}}>
+      <Text style={{fontSize: 20, fontWeight: '600', padding: 14}}>Thank you for your feedback!</Text>
+      <View style={{flex: 1, flexDirection:'row',}}>
+        <TouchableOpacity onPress={() => setState('')} style={{alignItems: 'center', width: 105}}>
+        <Text style={{fontSize: 50}}>😊</Text>
+        <Text>Very Satisfied</Text>
+        </TouchableOpacity>
+      </View> 
+    </View>
+        )
+    }
   return (
     <View>
-        {home()}
+        {state === "" && home()}
+        {state === "verySatisfied" && verySatisfied()}
+        {state === "verySad" && verySad()}
+        {state === "notBad" && notBad()}
     </View>
   )
 }
