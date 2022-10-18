0330b0fc59d998825e2e392c786342de049ba585
jngnandan <jngnandan@gmail.com>
2022-10-18 15:09:24 +0100

    function display added

--- views/Home.js
+++ views/Home.js
@@ -3,11 +3,11 @@ import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 import React from 'react'
 
 const Home = () => {
-    const [satisfied, setSatisfied] = React.useState(false)
+    const [state, setState] = React.useState('very satisfied')
 
-
-  return (
-    <View style={{height: 200, backgroundColor: 'white', width: '90%', flexDirection:'column', justifyContent: 'center', alignItems: 'center', borderRadius: 6}}>
+    const home = () => {
+        return (
+     <View style={{height: 200, backgroundColor: 'white', width: '94%', flexDirection:'column', justifyContent: 'center', alignItems: 'center', borderRadius: 6}}>
       <Text style={{fontSize: 20, fontWeight: '600', padding: 14}}>How satisfyed are you with our customer support performance?</Text>
       <View style={{flex: 1, flexDirection:'row',}}>
         <TouchableOpacity style={{alignItems: 'center', width: 105}}>
@@ -22,8 +22,14 @@ const Home = () => {
         <Text style={{fontSize: 50}}>😟</Text>
         <Text>Not Bad</Text>
         </TouchableOpacity>
-      </View>
-      
+      </View> 
+    </View>
+        )
+    }
+
+  return (
+    <View>
+        {home()}
     </View>
   )
 }
